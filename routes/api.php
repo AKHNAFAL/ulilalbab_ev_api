cf5638deefffb0b5e22b6fc0a66c5da806aa0318
AKHNAFAL <22523179@students.uii.ac.id>
2024-10-04 15:46:37 +0700

    Add new routes for attendance management and admin monitoring

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserDataController;
 use App\Http\Controllers\Meetings\MeetingController;
 use App\Http\Controllers\Meetings\LocationController;
+use App\Http\Controllers\Attendance\CheckInController;
+use App\Http\Controllers\Attendance\CheckOutController;
+use App\Http\Controllers\Attendance\CheckUserActiveController;
+use App\Http\Controllers\Attendance\HasOngoingMeetingController;
+use App\Http\Controllers\Attendance\UserAttendanceHistoryController;
+use App\Http\Controllers\Attendance\AdminAttendanceMonitoringController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::get('/user/has-ongoing-meeting', HasOngoingMeetingController::class);
+    Route::post('/user/check-in', CheckInController::class);
+    Route::post('/user/check-out', CheckOutController::class);
+    Route::get('/user/check-user-in-meeting', CheckUserActiveController::class);
+    Route::get('/user/attendances-history', UserAttendanceHistoryController::class);
+
 });
 
 Route::middleware(['auth:sanctum', 'adminAccess'])->group(function () {
@@ -19,7 +31,9 @@ Route::middleware(['auth:sanctum', 'adminAccess'])->group(function () {
     Route::get('meetings/ongoing', [MeetingController::class, 'ongoingMeetings']);
     Route::get('meetings/completed', [MeetingController::class, 'completedMeetings']);
     Route::get('meetings/upcoming', [MeetingController::class, 'upcomingMeetings']);
-
+    Route::get('meetings/{meetingId}/attendance', [AdminAttendanceMonitoringController::class, 'monitorMeetingAttendance']);
+    
+    
     Route::apiResource('meetings', MeetingController::class); 
     Route::apiResource('users', UserDataController::class);
     Route::apiResource('locations', LocationController::class);
